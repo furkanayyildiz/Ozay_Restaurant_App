80ad4a173f08b94db21062b8dfc5197ee3152cf5
Furkan Ayyıldız <furkan199905@gmail.com>
2023-05-01 00:32:48 +0300

    carousel image update

--- lib/products/widget/carousel_slider/category_model.dart
+++ lib/products/widget/carousel_slider/category_model.dart
@@ -16,22 +16,22 @@ class Category extends Equatable {
     Category(
       name: 'Pizza',
       image:
-          'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+          'https://www.unileverfoodsolutions.com.tr/dam/global-ufs/mcos/TURKEY/calcmenu/recipes/TR-recipes/desserts-&-bakery/karışık-pizza/main-header.jpg',
     ),
     Category(
       name: 'Burger',
       image:
-          'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+          "https://cdn.yemek.com/uploads/2023/03/burger-king-menu-fiyatlari-2023-3.jpg",
     ),
     Category(
-      name: 'Pasta',
+      name: 'Kebap',
       image:
-          'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+          'https://cdn.yemek.com/mnresize/940/940/uploads/2016/05/adana-kebap-one-cikan.jpg',
     ),
     Category(
       name: 'Salad',
       image:
-          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+          'https://d17wu0fn6x6rgz.cloudfront.net/img/w/tarif/ogt/beyaz-peynirli-akdeniz-salatasi.webp',
     ),
   ];
 }
